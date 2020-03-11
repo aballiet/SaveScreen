1d12a06e01d5d49c4ead2d7e812d477a74f62938
aballiet <antoine.balliet@gmail.com>
2020-03-11 18:34:48 +0100

    add persistent notification :postal_horn:
    Persistent notification to know when app is running and open it faster

--- app/src/main/java/com/balliet/screenoff/MainActivity.java
+++ app/src/main/java/com/balliet/screenoff/MainActivity.java
@@ -1,12 +1,19 @@
 package com.balliet.screenoff;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.core.app.NotificationCompat;
 
 
 public class MainActivity extends Activity {
@@ -17,6 +24,8 @@ public class MainActivity extends Activity {
     private PowerManager powerManager;
     private PowerManager.WakeLock wakeLock;
     private int field = 0x00000020;
+
+    private NotificationManager mNotificationManager;
 
 
     @Override
@@ -42,6 +51,7 @@ public class MainActivity extends Activity {
                     wakeLock.acquire();
                     status.setTextColor(Color.GREEN);
                     status.setText("Current status : on");
+                    showNotification("ScreenSave is running", "Use the proximity sensor to turn off the screen");
                 }
             }
         });
@@ -52,10 +62,42 @@ public class MainActivity extends Activity {
                     wakeLock.release();
                     status.setText("Current status : off");
                     status.setTextColor(Color.RED);
+                    mNotificationManager.cancel("screensave", 0);
                 }
             }
         });
+    }
 
+    public void showNotification(String title, String message) {
+        mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("screen_save_channel",
+                    "screensave",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("description");
+            mNotificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "screen_save_channel")
+                .setSmallIcon(R.mipmap.ic_launcher) // notification icon
+                .setContentTitle(title) // title for notification
+                .setContentText(message)// message for notification
+                .setOngoing(true);
+
+        Intent intent = newLauncherIntent(getApplicationContext());
+
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(pi);
+        mNotificationManager.notify("screensave", 0, mBuilder.build());
+    }
+
+    public static Intent newLauncherIntent(final Context context) {
+        final Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        return intent;
     }
 }
 
